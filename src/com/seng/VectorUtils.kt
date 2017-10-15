dd2c6833b800ebbd522b4d2691d3b29902923b1a
Adel Khial <adel.khial@gmail.com>
2017-10-15 14:12:18 +0100

    Added tests and JavaDoc modifications.

--- src/com/seng/VectorUtils.kt
+++ src/com/seng/VectorUtils.kt
@@ -1,18 +1,25 @@
 package com.seng
 
+/**
+ * Sorts the array according to the natural order of its elements.
+ * Uses a very un-optimised implementation of bubble sort, should only be used
+ * for small arrays.
+ * Please use [Array.sort] instead.
+ */
 fun Array<Int>.sort(): Array<Int> {
-    for (i in this.indices) {
-        for (j in this.indices) {
+    for (i in this.indices)
+        for (j in this.indices)
             if (this[j] > this[i]) {
                 val s = this[j]
                 this[j] = this[i]
                 this[i] = s
             }
-        }
-    }
     return this
 }
 
+/**
+ * Adds all elements in an int array to the elements of another and returns a new array.
+ */
 fun Array<Int>.add(other: Array<Int>): Array<Int> {
     assert(this.size == other.size)
     val result = Array(this.size, {0})
@@ -20,4 +27,37 @@ fun Array<Int>.add(other: Array<Int>): Array<Int> {
         result[i] = this[i] + other[i]
     }
     return result
+}
+
+/**
+ * Inverts the order of an array's elements. Please use [Array.reverse] instead.
+ */
+fun Array<Int>.invert(): Array<Int> {
+    val result = Array(this.size, {0})
+    for(i in this.indices) {
+        result[this.size - (i+1)] = this[i]
+    }
+    return result
+}
+
+/**
+ * Returns a [Pair] containing the minimum and maximum of an array.
+ *
+ * @return a [Pair] containing the minimum in the first and maximum in the second,
+ * or (null, null) for an empty array
+ */
+fun Array<Int>.getMinMax(): Pair<Int?, Int?> {
+    val min = this.min()
+    val max = this.max()
+
+    return Pair(min, max)
+}
+
+/**
+ *  Applies the given function on all elements of the array.
+ *
+ *  @param func the function to apply
+ */
+fun Array<Int>.apply(func: (value: Int) -> Int): Array<Int> {
+    return this.map(func).toTypedArray()
 }
--- test/VectorTest.kt
+++ test/VectorTest.kt
@@ -1,6 +1,6 @@
-import com.seng.add
-import com.seng.sort
+import com.seng.*
 import org.junit.Test
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertArrayEquals
 
 class VectorTest {
@@ -15,6 +15,42 @@ class VectorTest {
         assertArrayEquals(
                 arrayOf(2, 4, 6, 8, 10),
                 arrayOf(1, 1, 1, 1, 1).add(arrayOf(1, 3, 5, 7, 9))
+        )
+    }
+
+    @Test fun testInvert() {
+        assertArrayEquals(
+                arrayOf(5, 4, 3, 2, 1),
+                arrayOf(1, 2, 3, 4, 5).invert()
+        )
+    }
+
+    @Test fun testMinMaxNormal() {
+        assertEquals(
+                Pair(1, 10),
+                arrayOf(3, 5, 1, 8, 10).getMinMax()
+        )
+    }
+
+    @Test fun testMinMaxEmpty() {
+        assertEquals(
+                Pair(null, null),
+                arrayOf<Int>().getMinMax()
+        )
+    }
+
+    @Test fun testMinMaxOne() {
+        assertEquals(
+                Pair(10, 10),
+                arrayOf(10).getMinMax()
+        )
+    }
+
+    @Test fun testApply() {
+        val result = arrayOf(1, 2, 3, 4, 5).apply { it*2 }
+        assertArrayEquals(
+                arrayOf(2, 4, 6, 8, 10),
+                result
         )
     }
 }
